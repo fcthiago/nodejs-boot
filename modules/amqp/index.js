d88e66ba4dfdf480b4b1106c2cb918f94ba5b6da
brunogarciarana@gmail.com <brunogarciarana@gmail.com>
2019-11-04 14:10:48 -0300

    Created AmqpConfigurator

--- modules/amqp/index.js
+++ modules/amqp/index.js
@@ -1,8 +1,6 @@
-const { listModules } = require('awilix');
-const path = require("path");
-
 const RabbitMQAmqpProvider = require("./providers/RabbitMQAmqpProvider");
 const ServiceBusAmqpProvider = require("./providers/ServiceBusAmqpProvider");
+const AmqpConfigurator = require("./configurations/AmqpConfigurator");
 
 module.exports = async (container) => {
     const application = container.resolve("application");
@@ -18,26 +16,9 @@ module.exports = async (container) => {
     }
 
     if(ServiceBusAmqpProvider.checkConfiguration(application) || RabbitMQAmqpProvider.checkConfiguration(application)){
-        await configure(application, container);
+        new AmqpConfigurator().configure(application, container);
     }
 };
 
-async function configure(application, container) {
-    const inBrokers = listModules(path.join(path.dirname(require.main.filename), "../",application.node_boot.modules.inBrokers));
 
-    for(const key in inBrokers) {
-        const className = inBrokers[key].name;
-        const listen = container.resolve(convertToCamelCase(className)).listen;
 
-        if (listen == undefined)
-            throw new Error(className + " did not implement 'listen' method");
-
-        container.resolve(convertToCamelCase(className)).listen();
-    }
-}
-
-function convertToCamelCase(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-        return index == 0 ? word.toLowerCase() : word.toUpperCase();
-    }).replace(/\s+/g, '');
-}
