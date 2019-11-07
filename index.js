f6f478f3e6cfcc41c72d49371bbf5f013eeacc0b
dmedinanf <medinad2211@hotmail.com>
2019-11-07 09:22:59 -0300

    Removed node_boot validation

--- index.js
+++ index.js
@@ -14,44 +14,42 @@ module.exports = class NodeBoot {
     }
 
     async start() {
-        if (!node_boot) {
+        const { node_boot } = this.appConfig;
 
-            const { node_boot } = this.appConfig;
+        try {
+            this.appConfig = deepmerge(this.appConfig, require('../../' + node_boot.application_path));
+        } catch (e) {}
 
-            try {
-                this.appConfig = deepmerge(this.appConfig, require('../../' + node_boot.application_path));
-            } catch (e) {}
+        let nodeBootModulesResolved = [];
+        Object.values(this.appConfig.node_boot.modules).forEach((pathString) => {
+            nodeBootModulesResolved.push(path.join(path.dirname(require.main.filename), "../", pathString));
+        });
+        let modules = [path.join(__dirname, '/**/*.js')].concat(Object.values(nodeBootModulesResolved));
 
-            let nodeBootModulesResolved = [];
-            Object.values(this.appConfig.node_boot.modules).forEach((pathString) => {
-                nodeBootModulesResolved.push(path.join(path.dirname(require.main.filename), "../", pathString));
-            });
-            let modules = [path.join(__dirname, '/**/*.js')].concat(Object.values(nodeBootModulesResolved));
-
-            this.container.loadModules(modules, {
-                formatName: 'camelCase',
-                resolverOptions: {
-                    lifetime: Lifetime.SINGLETON,
-                }
-            });
-
-            this.container.register({
-                application: asValue(this.appConfig)
-            });
-
-
-            //Logging config
-            const logger = this.container.resolve('logger');
-            logger.level = application.logging.level;
-
-            //Loading extra modules from NodeBoot
-            const modulesFolder = path.join(__dirname, "/modules");
-            for (const file of fs.readdirSync(modulesFolder)) {
-                let module = require(require.resolve("./modules/" + file));
-                await module(this.container);
+        this.container.loadModules(modules, {
+            formatName: 'camelCase',
+            resolverOptions: {
+                lifetime: Lifetime.SINGLETON,
             }
+        });
 
-            this.container.resolve('expressWebServer').start(this.container);
+        this.container.register({
+            application: asValue(this.appConfig)
+        });
+
+
+        //Logging config
+        const logger = this.container.resolve('logger');
+        logger.level = application.logging.level;
+
+        //Loading extra modules from NodeBoot
+        const modulesFolder = path.join(__dirname, "/modules");
+        for (const file of fs.readdirSync(modulesFolder)) {
+            let module = require(require.resolve("./modules/" + file));
+            await module(this.container);
         }
+
+        this.container.resolve('expressWebServer').start(this.container);
     }
+
 };
